1fb9acb3b4de027098a01ee90086ba7209c87c89
Yen Ling Peng <s0890003@gmail.com>
2020-09-30 16:29:40 +0800

    modify the script

--- run_annotation.sh
+++ run_annotation.sh
@@ -1,53 +1,42 @@
 #!/bin/bash
-#PBS -q ntu192G
-#PBS -l select=1:ncpus=5
-#PBS -P MST109178
-#PBS -W group_list=MST109178
-#PBS -N gatk4.1_ANNOVAR
-#PBS -o /work2/lynn88065/GATK/Inputs/20200721_GATK_pipeline/annotation.out
-#PBS -e /work2/lynn88065/GATK/Inputs/20200721_GATK_pipeline/annotation.err
-#PBS -M s0890003@gmail.com
-#PBS -m e
 
-date="20200721"
-clinical="GATK"
-disease="pipeline"
-sample_name="NA12878_S38_panel"
-OUTPUT_PATH="/work2/lynn88065/GATK/Outputs/${date}_${clinical}_${disease}/${sample_name}"
-
-mkdir -p ${OUTPUT_PATH}/${sample_name}_hg19
-cd ${OUTPUT_PATH}/${sample_name}_hg19
-VCF=${OUTPUT_PATH}/${sample_name}.bwamem.marked.recal.pass1.indexed.filtered.haplotype.SnpIndel.vcf.gz
-
-#liftover from hg38 to hg19 and normalized
 Picard="/pkg/biology/Picard/Picard_v2.18.11/picard.jar"
 VT_PATH="/project/GP1/u3710062/AI_SHARE/software/vt-0.57721/vt"
+ANNOVAR="/pkg/biology/ANNOVAR/ANNOVAR_20191024/table_annovar.pl"
+humandb="/project/GP1/u3710062/AI_SHARE/shared_scripts/ANNOVAR/humandb/"
 TODAY=`date +%Y%m%d%H%M`
-logfile=./${TODAY}_run.log
+logfile=./${TODAY}_gatk_run.log
 exec 3<&1 4<&2
 exec >$logfile 2>&1
 set -euo pipefail
 set -x
 
+# Basic info
+SAMPLE_ID=$1
+VCF=$2
+OUTPUT_PATH=$3
+CHAIN_FILE=$4
+REF_GENOME_PATH=$5
+
+cd $OUTPUT_PATH
+
+# Liftover VCF (from hg38 to hg19)
 java -jar ${Picard} LiftoverVcf \
       I=${VCF} \
-      O=${sample_name}_hg19.hard.filtered.liftover.vcf \
-      CHAIN=/project/GP1/u3710062/AI_SHARE/reference/Liftover/hg38ToHg19.over.chain.gz \
-      REJECT=${sample_name}_hg19.rejected.variants.vcf \
-      R=/project/GP1/u3710062/AI_SHARE/reference/GATK_bundle/hg19/ucsc.hg19.fasta
-                                   
-$VT_PATH decompose -s -o ${sample_name}_hg19.decomposed.vcf ${sample_name}_hg19.hard.filtered.liftover.vcf
-$VT_PATH normalize -o ${sample_name}_hg19.norm.vcf -r /project/GP1/u3710062/AI_SHARE/reference/GATK_bundle/hg19/ucsc.hg19.fasta ${sample_name}_hg19.decomposed.vcf
+      O=${SAMPLE_ID}_hg19.hard.filtered.liftover.vcf \
+      CHAIN=$CHAIN_FILE \
+      REJECT=${SAMPLE_ID}_hg19.rejected.variants.vcf \
+      R=$REF_GENOME_PATH
+
+# VT-normalization                                   
+$VT_PATH decompose -s -o ${SAMPLE_ID}_hg19.decomposed.vcf ${SAMPLE_ID}_hg19.hard.filtered.liftover.vcf
+$VT_PATH normalize -o ${SAMPLE_ID}_hg19.norm.vcf -r $REF_GENOME_PATH ${SAMPLE_ID}_hg19.decomposed.vcf
 
 # run Annotation
-para="ANNOVAR_${sample_name}"
-ANNOVAR="/pkg/biology/ANNOVAR/ANNOVAR_20191024/table_annovar.pl"
-normalized_VCF=${sample_name}_hg19.norm.vcf
-humandb="/project/GP1/u3710062/AI_SHARE/shared_scripts/ANNOVAR/humandb/"
+$ANNOVAR ${SAMPLE_ID}_hg19.norm.vcf $humandb -buildver hg19 -out ${SAMPLE_ID} \
+        -remove -protocol refGene,TaiwanBiobank-official,TaiwanBiobank993WGS,avsnp150,exac03,exac03nonpsych,clinvar_20190305,gnomad211_exome,gnomad211_genome,cosmic_coding_GRCh37_v91,cosmic_noncoding_GRCh37_v91 -operation gx,f,f,f,f,f,f,f,f,f,f -nastring . -vcfinput -polish
 
-$ANNOVAR $normalized_VCF $humandb -buildver hg19 -out ${para} -remove -protocol refGene,TaiwanBiobank-official,TaiwanBiobank993WGS,avsnp150,exac03,exac03nonpsych,clinvar_20190305,gnomad211_exome,gnomad211_genome,cosmic_coding_GRCh37_v91,cosmic_noncoding_GRCh37_v91 -operation gx,f,f,f,f,f,f,f,f,f,f -nastring . -vcfinput -polish
-
-# filter---exonic/splicing and nonsynonymous
-head -n 1 ANNOVAR_${sample_name}.hg19_multianno.txt > ${sample_name}.filtered.annotation.txt
-grep -e "exonic" -e "splicing" ANNOVAR_${sample_name}.hg19_multianno.txt >> ${sample_name}.filtered.annotation.txt
+# Filtration records (exonic/splicing)
+head -n 1 ANNOVAR_${SAMPLE_ID}.hg19_multianno.txt > ${SAMPLE_ID}.filtered.annotation.txt
+grep -e "exonic" -e "splicing" ${SAMPLE_ID}.hg19_multianno.txt >> ${SAMPLE_ID}.filtered.annotation.txt
 
